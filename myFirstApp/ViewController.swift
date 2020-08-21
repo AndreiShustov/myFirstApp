9b1d109302c1ab0332baf6ce641345eef94be13d
Andrei Shustov <shustov_a@mail.ru>
2020-08-21 21:48:33 +1000

    first commit

---
+++ myFirstApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Андрей Шустов on 21.08.2020.
+//  Copyright © 2020 Andrei Shustov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //gfhfghfghg
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
